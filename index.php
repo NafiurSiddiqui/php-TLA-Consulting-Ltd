9ac0d68b4df4a5304deff7422d4d813f6c15ecd2
Nafi R <saejim91@gmail.com>
2023-04-14 23:02:18 +0600

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//REUSABLE CONTENT
+
+class Page
+{
+    public $content;
+    public $title = "TLA Consulting Pty Ltd";
+    public $keywords = "TLA Consulting. GLobal consultation center for your IT.";
+    public $buttons = [
+        "Home" => "home.php",
+        "Services"=> "services.php",
+        "Contact" => "contact.php",
+        "Site Map" => "map.php"
+    ]
+
+
+}
